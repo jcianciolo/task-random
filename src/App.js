0378df4503b2cada07b53adf2728e2e5ecb45ccd
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2021-09-01 13:16:40 -0700

    added new task form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react';
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -24,14 +26,31 @@ const App = () => {
     }
 ])
 
-  const deleteTask = (id) => {
-    console.log('delete', id)
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
   }
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+        )
+    )
+  }
   return (
     <div className="container">
-      <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> 
+        ) : (
+          'No Tasks'
+        )}
     </div>
   );
 }
